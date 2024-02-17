a6e1617c17f6d29b15aaf14686bcaa093ddeeb86
samham95 <samhamvz@gmail.com>
2024-02-16 18:15:20 -0600

    conditionally render navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -14,68 +14,57 @@ const Navbar = () => {
         else setActiveSession(false)
     }, [location])
 
-    if (activeSession) {
-        return (
-            <div>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark my-navbar-dark">
-                    <Link className="navbar-brand" to="/">
-                        <img src="/fuel_logo.png" width="40" height="40" alt="" />
-                    </Link>
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark my-navbar-dark">
+                <Link className="navbar-brand" to="/">
+                    <img src="/fuel_logo.png" width="40" height="40" alt="" />
+                </Link>
 
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item active">
-                                <Link className="nav-link navLink" to="/">Home <span className="sr-only">(current)</span></Link>
-                            </li>
-                            <li className="nav-item active">
-                                <Link className="nav-link navLink" to="/">About</Link>
-                            </li>
-                            <li className="nav-item dropdown active">
-                                <a className="nav-link dropdown-toggle navLink" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Profile
-                                </a>
-                                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <Link className="dropdown-item navLink" to="/profile">My Profile</Link>
-                                    <div className="dropdown-divider"></div>
-                                    <Link className="dropdown-item navLink" to="/">New Fuel Quote</Link>
-                                    <Link className="dropdown-item navLink" to="/">Quote History</Link>
-                                </div>
-                            </li>
-                        </ul>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <Link className="nav-link navLink" to="/">Home <span className="sr-only">(current)</span></Link>
+                        </li>
+                        {/* Ternary operator to display links based on activeSession */}
+                        {activeSession ? (
+                            <>
+                                <li className="nav-item active">
+                                    <Link className="nav-link navLink" to="/">About</Link>
+                                </li>
+                                <li className="nav-item dropdown active">
+                                    <a className="nav-link dropdown-toggle navLink" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Profile
+                                    </a>
+                                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <Link className="dropdown-item navLink" to="/profile">My Profile</Link>
+                                        <div className="dropdown-divider"></div>
+                                        <Link className="dropdown-item navLink" to="/">New Fuel Quote</Link>
+                                        <Link className="dropdown-item navLink" to="/">Quote History</Link>
+                                    </div>
+                                </li>
+                            </>
+                        ) : (
+                            <>
+                                <li className="nav-item active">
+                                    <Link className="nav-link navLink" to="/login">Login</Link>
+                                </li>
+                                <li className="nav-item active">
+                                    <Link className="nav-link navLink" to="/register">Register</Link>
+                                </li>
+                            </>
+                        )}
+                    </ul>
+                    {/* Conditional rendering for Logout based on activeSession */}
+                    {activeSession && (
                         <div className="ml-auto">
                             <Logout />
                         </div>
-                    </div>
-                </nav>
-            </div>
-        );
-    }
-    else {
-        return (
-            <div>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark my-navbar-dark">
-                    <Link className="navbar-brand" to="/">
-                        <img src="/fuel_logo.png" width="40" height="40" alt="" />
-                    </Link>
-
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item active">
-                                <Link className="nav-link navLink" to="/">Home <span className="sr-only">(current)</span></Link>
-                            </li>
-                            <li className="nav-item active">
-                                <Link className="nav-link navLink" to="/login">Login</Link>
-                            </li>
-                            <li className="nav-item active">
-                                <Link className="nav-link navLink" to="/register">Register</Link>
-                            </li>
-
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        )
-    }
+                    )}
+                </div>
+            </nav>
+        </div>
+    );
 };
 
 export default Navbar;
