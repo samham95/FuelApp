004db26b03d8b4ab845d09700f6df2d4a8ba3f80
samham95 <samhamvz@gmail.com>
2024-02-22 23:11:30 -0600

    fixed navbar not showing when logged out

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,88 +1,73 @@
 import { React, useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import Logout from './Logout';
+import { authClient } from './apiClient';
 import './styles.css';
 
 const Navbar = () => {
     let location = useLocation()
-    const token = localStorage.getItem('token');
-    const [activeSession, setActiveSession] = useState(token)
+    const [activeSession, setActiveSession] = useState(false)
     useEffect(() => {
-        let currState = localStorage.getItem('token');
-        if (currState) setActiveSession(true)
-        else setActiveSession(false)
-    }, [location])
+        const token = localStorage.getItem('token');
+        const username = localStorage.getItem('username');
+        authClient(token).post('/auth', { username })
+            .then((res) => {
+                const auth = res.data.isAuthorized;
+                if (auth) setActiveSession(true);
+                else throw new Error;
+            })
+            .catch(() => {
+                setActiveSession(false);
+            })
+    }, [location]);
 
-    if (activeSession) {
-        return (
-            <div>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark my-navbar-dark">
-                    <Link className="navbar-brand" to="/">
-                        <img src="/fuel_logo2.png" width="40" height="40" alt="" />
-                    </Link>
-
-                    <div className="collapse navbar-collapse" id="collapseNavbar">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item active">
-                                <Link className="nav-link navLink" to="/">Home <span className="sr-only">(current)</span></Link>
-                            </li>
-                            {activeSession ? (
-                                <>
-                                    <li className="nav-item active">
-                                        <Link className="nav-link navLink" to="/">About</Link>
-                                    </li>
-                                    <li className="nav-item dropdown active">
-                                        <a className="nav-link dropdown-toggle navLink" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            Profile
-                                        </a>
-                                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                            <Link className="dropdown-item navLink" to="/profile">My Profile</Link>
-                                            <div className="dropdown-divider"></div>
-                                            <Link className="dropdown-item navLink" to="/quote">New Fuel Quote</Link>
-                                            <Link className="dropdown-item navLink" to="/quote/history">Quote History</Link>
-                                        </div>
-                                    </li>
-                                </>
-                            ) : (
-                                <>
-                                    <li className="nav-item active">
-                                        <Link className="nav-link navLink" to="/login">Login</Link>
-                                    </li>
-                                    <li className="nav-item active">
-                                        <Link className="nav-link navLink" to="/register">Register</Link>
-                                    </li>
-                                </>
-                            )}
-                        </ul>
-                        {activeSession && (
-                            <div className="ml-auto">
-                                <Logout />
-                            </div>
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark my-navbar-dark">
+                <Link className="navbar-brand" to="/">
+                    <img src="/fuel_logo2.png" width="40" height="40" alt="" />
+                </Link>
+                <div className="collapse navbar-collapse" id="collapseNavbar">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <Link className="nav-link navLink" to="/">Home <span className="sr-only">(current)</span></Link>
+                        </li>
+                        {activeSession ? (
+                            <>
+                                <li className="nav-item active">
+                                    <Link className="nav-link navLink" to="/">About</Link>
+                                </li>
+                                <li className="nav-item dropdown active">
+                                    <a className="nav-link dropdown-toggle navLink" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Profile
+                                    </a>
+                                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <Link className="dropdown-item navLink" to="/profile">My Profile</Link>
+                                        <div className="dropdown-divider"></div>
+                                        <Link className="dropdown-item navLink" to="/quote">New Fuel Quote</Link>
+                                        <Link className="dropdown-item navLink" to="/quote/history">Quote History</Link>
+                                    </div>
+                                </li>
+                            </>
+                        ) : (
+                            <>
+                                <li className="nav-item active">
+                                    <Link className="nav-link navLink" to="/login">Login</Link>
+                                </li>
+                                <li className="nav-item active">
+                                    <Link className="nav-link navLink" to="/register">Register</Link>
+                                </li>
+                            </>
                         )}
-                    </div>
-                </nav>
-            </div>
-        );
-    }
-    else {
-        return (
-            <div>
-                <nav className='nav-home'>
-                    <div className='nav-logo'>
-                        <a href='/'>
-                            <img src='fuel_logo2.png' height='40px' width='40px'></img>
-                        </a>
-                    </div>
-                    <ul className='nav-link'>
-                        <li className='link'><a href='/'>Home</a></li>
-                        <li className='link' id='linkA'><a href='#containers'>About Us</a></li>
                     </ul>
-                    <Link to='/quote'>
-                        <button className="home-login">Login</button>
-                    </Link>
-                </nav>
-            </div>
-        );
-    };
-}
+                    {activeSession && (
+                        <div className="ml-auto">
+                            <Logout />
+                        </div>
+                    )}
+                </div>
+            </nav>
+        </div>
+    );
+};
 export default Navbar;
