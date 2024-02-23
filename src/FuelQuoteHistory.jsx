438fd272d5107421de119d05040d11cd9e03417e
BillyO24 <100812335+BillyO24@users.noreply.github.com>
2024-02-23 17:22:03 -0600

    Fixed Table Style Issues
    Adjusted width of table and fixed scrolling issue so head of table remains fixed while body scrolls.

--- src/FuelQuoteHistory.jsx
+++ src/FuelQuoteHistory.jsx
@@ -42,41 +42,39 @@ const FuelQuoteHistory = () => {
                 <div className="pageTitle">
                     <h1 className="header1">Fuel Quote History</h1>
                 </div>
-                <div className="fuelHistory">
-                    <table className="fuelTable">
-                        <thead>
-                            <tr>
-                                <th className="text-center">Gallons Requested</th>
-                                <th className="text-center">Delivery Address</th>
-                                <th className="text-center">Delivery Date</th>
-                                <th className="text-center">Suggested Price (per gallon)</th>
-                                <th className="text-center">Total Due</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                quotes !== undefined && quotes.length > 0 ? (
-                                    quotes.map((q, indx) => {
-                                        const address = `${q.address.street}, ${q.address.city}, ${q.address.state}, ${q.address.zip}`;
-                                        return (
-                                            <tr key={indx}>
-                                                <td>{q.gallonsRequested}</td>
-                                                <td>{address}</td>
-                                                <td>{q.deliveryDate}</td>
-                                                <td>{`$${q.suggestedPricePerGallon.toFixed(2)}`}</td>
-                                                <td>{`$${q.totalDue.toFixed(2)}`}</td>
-                                            </tr>
-                                        );
-                                    })
-                                ) : (
-                                    <tr><td colSpan="5">No quotes available</td></tr>
-                                )
-                            }
+                <table className="fuelTable">
+                    <thead>
+                        <tr>
+                            <th className="text-center">Gallons Requested</th>
+                            <th className="text-center">Delivery Address</th>
+                            <th className="text-center">Delivery Date</th>
+                            <th className="text-center">Suggested Price (per gallon)</th>
+                            <th className="text-center">Total Due</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            quotes !== undefined && quotes.length > 0 ? (
+                                quotes.map((q, indx) => {
+                                    const address = `${q.address.street}, ${q.address.city}, ${q.address.state}, ${q.address.zip}`;
+                                    return (
+                                        <tr key={indx}>
+                                            <td>{q.gallonsRequested}</td>
+                                            <td>{address}</td>
+                                            <td>{q.deliveryDate}</td>
+                                            <td>{`$${q.suggestedPricePerGallon.toFixed(2)}`}</td>
+                                            <td>{`$${q.totalDue.toFixed(2)}`}</td>
+                                        </tr>
+                                    );
+                                })
+                            ) : (
+                                <tr><td colSpan="5">No quotes available</td></tr>
+                            )
+                        }
 
-                        </tbody>
-                    </table>
+                    </tbody>
+                </table>
                 </div>
-            </div>
         </>
     );
 }
