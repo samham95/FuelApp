434ab47590432838a08410caf500569ccb87bc7b
samham95 <samhamvz@gmail.com>
2024-03-19 13:25:59 -0500

    created fuel pricing class

---
+++ backend/src/pricingModule.js
@@ -0,0 +1,11 @@
+class FuelPricing {
+    constructor(username, state, gallons) {
+        this.username = username;
+        this.state = state;
+        this.gallons = gallons;
+    }
+
+    async getPricePerGallon() {
+        return 2.5;
+    }
+}
